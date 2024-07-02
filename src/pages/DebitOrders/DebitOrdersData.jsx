023235314e524c6e15bab4d4214881f1cce873db
devwasabi2 <Wandile.Nyembe@bbd.co.za>
2024-07-02 12:30:05 +0200

    added local debit orders list

---
+++ src/pages/DebitOrders/DebitOrdersData.jsx
@@ -0,0 +1,54 @@
+
+export const debitOrders = [
+    {
+      id: 1,
+      debitAccountName: 'food-retail',
+      creditAccountName: 'retail-bank',
+      creationDate: '2024/06/29',
+      debitOrderAmount: 10000,
+      senderRef: 'ref-C678234234',
+      receiverRef: 'ref-C678234234',
+      disabled: false
+    },
+    {
+      id: 2,
+      debitAccountName: 'electronics-retailer',
+      creditAccountName: 'retail-bank',
+      creationDate: '2024/06/30',
+      debitOrderAmount: 50000,
+      senderRef: 'ref-C786123456',
+      receiverRef: 'ref-C786123456',
+      disabled: false
+    },
+    {
+      id: 3,
+      debitAccountName: 'short-term-lender',
+      creditAccountName: 'commercial-bank',
+      creationDate: '2024/07/01',
+      debitOrderAmount: 8000,
+      senderRef: 'ref-D987654321',
+      receiverRef: 'ref-D987654321',
+      disabled: true
+    },
+    {
+      id: 4,
+      debitAccountName: 'health-insurance',
+      creditAccountName: 'retail-bank',
+      creationDate: '2024/07/02',
+      debitOrderAmount: 500,
+      senderRef: 'ref-E543210987',
+      receiverRef: 'ref-E543210987',
+      disabled: false
+    },
+    {
+      id: 5,
+      debitAccountName: 'home-loans',
+      creditAccountName: 'retail-bank',
+      creationDate: '2024/07/02',
+      debitOrderAmount: 2500,
+      senderRef: 'ref-E543210987',
+      receiverRef: 'ref-E543210987',
+      disabled: false
+    }
+];
+  
