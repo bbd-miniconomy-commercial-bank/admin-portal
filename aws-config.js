bb7a3ba43c0e95ee5c897e7307b0453e95f81168
devwasabi2 <Wandile.Nyembe@bbd.co.za>
2024-07-03 19:11:28 +0200

    added aws-config file

---
+++ aws-config.js
@@ -0,0 +1,11 @@
+export const awsConfig = {
+  Auth: {
+    Cognito: {
+      mandatorySignIn: true,
+      region: import.meta.env.VITE_AWS_REGION,
+      userPoolId: import.meta.env.VITE_AWS_USER_POOL_ID,
+      userPoolClientId: import.meta.env.VITE_AWS_USER_POOL_CLIENT_ID,
+      
+    },
+  },
+};
